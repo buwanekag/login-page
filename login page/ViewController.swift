7d17dcbc64b7933cd260029089dd66a5813afe91
buwanekag <buwanekagalpoththawela@Buwanekas-MBP.home>
2016-01-11 20:39:43 -0500

    Login Page
    Initial Commit

---
+++ login page/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  login page
+//
+//  Created by Buwaneka Galpoththawela on 1/10/16.
+//  Copyright © 2016 Buwaneka Galpoththawela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    //MARK - INTERACTIVE METHOD
+    
+    
+    
+    
+    
+    //MARK: - LIFE CYCLE METHOD
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
